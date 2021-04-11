20df41f9f6a870a9469d167057c958a4588e1a6c
rpnilesh08 <46248484+rpnilesh08@users.noreply.github.com>
2021-04-11 15:15:07 +0530

    Update developer.py

--- developer.py
+++ developer.py
@@ -14,16 +14,28 @@ class Developer:
         self.root.geometry("1300x680+0+0")
         self.root.title("Face Recognition System")
 
-        title_lbl = Label(text="Developer :- Nilesh Rajpurohit", font=("times new roman", 35, "bold"),
-                          bg="white", fg="gray")
-        title_lbl.place(x=0, y=0, width=1275, height=50)
+        right_frame = LabelFrame(self.root, bd=2, bg="white", relief=RIDGE,
+                                 font=("times new roman", 15, "bold"))
+        right_frame.place(x=0, y=0, width=1275, height=152)
 
-        img_left = Image.open(r"C:\Users\Abr79\PycharmProjects\pythonProject2\College images\dev.jpg")
+        title_lbl = Label(right_frame,text="Developer Name :- Nilesh Rajpurohit",
+                         anchor=CENTER,
+                         font=("times new roman", 30, "bold"),
+                         bg="white", fg="gray")
+        title_lbl.place(x=23, y=0, width=1275, height=70)
+
+        title_lbl = Label(right_frame,text="Contact info :- 809799352",
+                          anchor=CENTER,
+                          font=("times new roman", 30, "bold"),
+                          bg="white", fg="gray")
+        title_lbl.place(x=0, y=65, width=1275, height=70)
+
+        img_left = Image.open(r"D:\py\pythonProject2\College images\dev.jpg")
         img_left = img_left.resize((1275, 645), Image.ANTIALIAS)
         self.photoimg = ImageTk.PhotoImage(img_left)
 
         f_lbl = Label(self.root, image=self.photoimg)
-        f_lbl.place(x=0, y=50, width=1275, height=650)
+        f_lbl.place(x=0, y=150, width=1275, height=650)
 
 
 if __name__ == "__main__":
