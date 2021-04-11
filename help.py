bf870f9e7c406f0b0641ec7039ee5d761e068a61
rpnilesh08 <46248484+rpnilesh08@users.noreply.github.com>
2021-04-11 15:14:34 +0530

    Update help.py

--- help.py
+++ help.py
@@ -14,21 +14,22 @@ class Help_Desk:
         self.root.geometry("1300x680+0+0")
         self.root.title("Face Recognition System")
 
-        title_lbl = Label(text="Help Desk", font=("times new roman", 35, "bold"),
+        title_lbl = Label(self.root,text="Help Desk",
+                         font=("times new roman", 35, "bold"),
                           bg="white", fg="purple")
         title_lbl.place(x=0, y=0, width=1275, height=50)
 
-        img_left = Image.open(r"C:\Users\Abr79\PycharmProjects\pythonProject2\College images\help 1.jpg")
+        img_left = Image.open(r"D:\py\pythonProject2\College images\help 1.jpg")
         img_left = img_left.resize((1275, 700), Image.ANTIALIAS)
         self.photoimg = ImageTk.PhotoImage(img_left)
 
         f_lbl = Label(self.root, image=self.photoimg)
         f_lbl.place(x=0, y=50, width=1275, height=650)
 
-        title = Label(text="Email :- rpnilesh88@gmail.com",relief = RIDGE,
+        title = Label(f_lbl,text="Email :- rpnilesh88@gmail.com",relief = RIDGE,
                       font=("times new roman", 35, "bold"),
                       fg="black")
-        title.place(x=330, y=440, width=660, height=50)
+        title.place(x=330, y=387, width=660, height=71)
 
 
 
